0c9cd5efbc4ddd9f83aa1fb761b38ed87fcbe79b
BernieRebolledo <bl.rebolledo@gmail.com>
2015-08-15 15:24:47 -0500

    cambiando titulo de categorias

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20150710231535) do
     t.boolean  "verified"
     t.string   "provider_uid"
     t.string   "password"
+    t.string   "phone"
+    t.string   "address"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
